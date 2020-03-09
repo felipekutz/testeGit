1da8c439b32164dfa93760f072f65936df94c3cf
Felipe Kutz <felipekutz@hotmail.com>
2020-03-09 11:08:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/',(req, res)=>{
   res.status(200).send({message: 'Olá Mundo estou funcionando!'});
 })
+app.get('/ola',(req, res)=>{
+  res.status(200).send({message: 'Olá eu sou a rota Olá'});
+})
 app.listen(3001, ()=>{
   console.log('Api rodando na porta 3001');
 })      
